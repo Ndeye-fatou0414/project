ddef6b61253cc6b8295d51a6876144fa7708be0b
Ndeye-fatou0414 <fatoundieguene98@gmail.com>
2026-01-21 17:26:38 +0000

    easy!

--- red-product-frontend/vite.config.js
+++ red-product-frontend/vite.config.js
@@ -1,8 +1,18 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+// export default defineConfig({
+//   plugins: [react(),
+
+//     tailwindcss()],
+
+//  base: "/project"
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [react()],
-
-  base: "/project",
+  base: "/",
 });
